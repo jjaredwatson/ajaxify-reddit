bdc20ce4a33e3f2276a45bac6777663e1334a311
Jared Watson <jjaredwatson@gmail.com>
2017-05-16 18:52:20 -0500

    cleaning up and making dev button active

--- js/main.js
+++ js/main.js
@@ -2,38 +2,38 @@
 var frontPage = 'https://www.reddit.com/.json';
 var developerPage = 'https://www.reddit.com/r/developer/.json';
 
-
-
-
 $(document).ready(function(){
+  // load main page
+  $.ajax({
+    method: "GET",
+    url: "https://www.reddit.com/.json",
+    success: onHomeLoad
+  });
 
-$.ajax({
-  method: "GET",
-  url: "https://www.reddit.com/.json",
-  success: onSuccess,
+// load dev page on dev button click
+  $('#developer').on('click', function() {
+    console.log("devbutton clicked.");
+    $.ajax({
+      method: "GET",
+      url: "https://www.reddit.com/r/developer/.json",
+      success: loadDevPage
+    });
+  });
+
 });
 
-// $.ajax({
-//   method: "GET",
-//   url: "https://www.reddit.com/r/developer/.json",
-//   success: onSuccess,
-// });
-
-
-function onSuccess(json) {
+function loadDevPage(json) {
   $('#content-box').empty();
-  console.log(json);
+  // console.log(json);
   for (var i = 0; i < json.data.children.length; i++) {
     var homeTitle = json.data.children[i].data.title;
-    var homeImage = json.data.children[i].data.preview.images["0"].resolutions["0"].url;
-    var devTitle = json.data.children[i].data.title;
-
+    // var homeImage = json.data.children[i].data.preview.images[0].resolutions[0].url;
+    // console.log(homeImage);
     var homePost = `<div class="row" id="boxes">
-      <div class="col-md-3" id="story-image"><img src="${homeImage}"></div>
-      <div class="col-md-6" id="story-title"><p>${homeTitle}</p></div>
-      <div class="col-md-3" id="comments-share"><button>comment</button></div>
+    <div class="col-md-6" id="story-title"><p>${homeTitle}</p></div>
+    <div class="col-md-3" id="comments-share"><button>comment</button></div>
     </div>
-    <hr>`
+    <hr>`;
 
     $('#content-box').append(homePost);
 
@@ -41,36 +41,30 @@ function onSuccess(json) {
 };
 
 
-//
-// $('#developer').on(click, developerPost(event) {
-//   $('#content-box').empty();
-//   event.preventDefault();
-//   $.ajax({
-//     method: "GET",
-//     url: "https://www.reddit.com/r/developer/.json",
-//     success: onSuccess,
-//   });
-// }
-//
-//   for (var i = 0; i < json.data.children.length; i++){
-//     var devTitle = json.data.children[i].data.title;
-//     var developerPost = `<div class="row" id="boxes">
-//       <div class="col-md-9" id="story-title"><p>${devTitle}</p></div>
-//       <div class="col-md-3" id="comments-share"><button>comment</button></div>
-//     </div>
-//     <hr>`
-//
-//     $('#content-box').append(developerPost);
-//   }
-//
-// });
-//
+
+function onHomeLoad(json) {
+  $('#content-box').empty();
+  // console.log(json);
+  for (var i = 0; i < json.data.children.length; i++) {
+    var homeTitle = json.data.children[i].data.title;
+    var homeImage = json.data.children[i].data.preview.images[0].resolutions[0].url;
+    // console.log(homeImage);
+    var homePost = `<div class="row" id="boxes">
+    <div class="col-md-3" id="story-image"><img src="${homeImage}"></div>
+    <div class="col-md-6" id="story-title"><p>${homeTitle}</p></div>
+    <div class="col-md-3" id="comments-share"><button>comment</button></div>
+    </div>
+    <hr>`;
+
+    $('#content-box').append(homePost);
+
+  }
+};
 
 
 
 
-//end of document
-});
+
 
 
 
